353f533c9f086825fae5dd2bc463fa28395a4827
ElijahCoding <cuiyeqing960904@gmail.com>
2020-08-17 20:49:15 +0300

    spa built

---
+++ resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav />
+
+        <div class="flex overflow-y-hidden flex-1">
+            <Sidebar />
+
+            <div class="overflow-x-hidden w-2/3">
+                <router-view :key="$route.fullPath"></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Nav from './Nav';
+
+export default {
+    components: {
+        Nav,
+    },
+}
+</script>
---
+++ resources/js/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Link from './modules/link';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {
+        Link
+    },
+    state: {
+
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    }
+});
